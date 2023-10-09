bfdaf8f8fccc948ec3656dccf2ca499a6686129f
Anikesh02 <140998878+Anikesh02@users.noreply.github.com>
2023-10-09 21:44:08 +0530

    Revert "Merge branch 'duplicate' of https://github.com/Anikesh02/Eldcare-webapp into duplicate"

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,33 +1,30 @@
-import React from 'react';
+import React from 'react'
 
 const Contact = () => {
-  return (
-    <section className="bg-blue-100 py-10">
-      <div className="container mx-auto max-w-screen-md px-4">
-        <h2 className="text-3xl font-bold text-center mb-6">Contact Us</h2>
-        <p className="text-lg font-light text-center mb-12">
-          Got a technical issue? Want to send feedback about a beta feature? Let us know.
-        </p>
-        <form className="space-y-8">
-          <div>
-            <label htmlFor="email" className="block font-semibold mb-1 text-gray-700">Your Email</label>
-            <input type="email" id="email" placeholder="example@gmail.com" className="w-full p-2 rounded border border-gray-300 placeholder-gray-400" />
-          </div>
+  return <section>
+    <div className="px-4 mx-auto max-w-screen-md">
+      <h2 className='heading text-center'>Contact Us</h2>
+      <p className="mb-8 lg:mb-16 font-light text-center text__para">Got a technical issue ? Want to send feedback about a beta feature ? Let us know</p>
+      <form action="#" className='space-y-8'>
+        <div>
+          <label htmlFor="email" className='form__label'>Your Email</label>
+          <input type="email" id='email' placeholder='example@gmail.com' className='form__input mt-1' />
+        </div>
 
-          <div>
-            <label htmlFor="subject" className="block font-semibold mb-1 text-gray-700">Subject</label>
-            <input type="text" id="subject" placeholder="Let us know how we can help you" className="w-full p-2 rounded border border-gray-300 placeholder-gray-400" />
-          </div>
+        <div>
+          <label htmlFor="subject" className='form__label'>Subject</label>
+          <input type="text" id='subject' placeholder='Let us know how we can help you' className='form__input mt-1' />
+        </div>
 
-          <div className="col-span-2">
-            <label htmlFor="message" className="block font-semibold mb-1 text-gray-700">Your Message</label>
-            <textarea rows={5} type="text" id="message" placeholder="Leave a comment..." className="w-full p-2 rounded border border-gray-300 placeholder-gray-400"></textarea>
-          </div>
-          <button type="submit" className="btn w-auto py-2 px-6 rounded bg-blue-500 text-white hover:bg-blue-700">Submit</button>
-        </form>
-      </div>
-    </section>
-  );
-};
+        <div className='sm:col-span-2'>
+          <label htmlFor="message" className='form__label'>Your Message</label>
+          <textarea rows={5} type="text" id='message' placeholder='Let a comment....' className='form__input mt-1' />
+        </div>
+        <button type='submit' className='btn rounded sm:w-fit'>Submit</button>
+      </form>
+    </div>
+  </section>
+  
+}
 
-export default Contact;
+export default Contact
--- frontend/src/pages/Notification.jsx
+++ frontend/src/pages/Notification.jsx
@@ -1,4 +1,9 @@
-import React from 'react';
+import React from 'react'
+// import { services } from '../assets/data/services'
+// import ServiceCard from '../components/Services/ServiceCard'
+// import NotificationPage from '../components/NotificationPage';
+import '../App.css';
+
 import Medicines from '../components/Medicines';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -13,27 +18,13 @@ const Notification = () => {
   };
 
   return (
-    <div className="bg-gray-100 min-h-screen">
-   
-
-      {/* Your Notifications heading */}
-      <div className="max-w-3xl mx-auto p-4 mt-4">
-        <h1 className="text-3xl font-bold mb-4 text-center">Your Notifications</h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-          <div className="bg-white p-4 rounded-lg shadow-lg">
-            <h2 className="text-xl font-bold mb-2">Appointments</h2>
-            <Appointments appointments={appointments} />
-          </div>
-          <div className="bg-white p-4 rounded-lg shadow-lg">
-            <h2 className="text-xl font-bold mb-2">Medicines</h2>
-            <Medicines medicines={medicines} />
-          </div>
-        </div>
-      </div>
-
+    <div className="App">
+      <Appointments appointments={appointments} />
+      <Medicines medicines={medicines} />
       <ToastContainer position="bottom-right" autoClose={3000} />
     </div>
   );
-};
+  
+}
 
-export default Notification;
+export default Notification
